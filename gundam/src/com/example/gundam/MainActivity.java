4b58eb4f65df3460bdd2049ff85307827f4f02f2
lisong <vampire08ms@tom.com>
2015-02-27 11:48:35 +0800

    跳转到MainActivity2页面

--- gundam/src/com/example/gundam/MainActivity.java
+++ gundam/src/com/example/gundam/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.gundam;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,7 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity {
 
 	public Button button = null;
 
@@ -20,6 +18,29 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				// NotificationManager nm = (NotificationManager)
+				// getSystemService(Context.NOTIFICATION_SERVICE);
+				// @SuppressWarnings("deprecation")
+				// Notification n = new Notification(R.drawable.ic_launcher,
+				// "Hello,there!", System.currentTimeMillis());
+				// n.flags = Notification.FLAG_AUTO_CANCEL;
+				//
+				// n.setLatestEventInfo(this, "Hello,there!",
+				// "Hello,there,I'm john.", null);
+				// nm.notify(1, n);
+
+				Intent intent = new Intent();
+				intent.setClass(MainActivity.this, MainActivity2.class);
+				startActivity(intent);
+
+			}
+		});
 	}
 
 	@Override
@@ -41,20 +62,4 @@ public class MainActivity extends Activity implements OnClickListener {
 		return super.onOptionsItemSelected(item);
 	}
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void onClick(View v) {
-
-		// NotificationManager nm = (NotificationManager)
-		// getSystemService(Context.NOTIFICATION_SERVICE);
-		// @SuppressWarnings("deprecation")
-		// Notification n = new Notification(R.drawable.ic_launcher,
-		// "Hello,there!", System.currentTimeMillis());
-		// n.flags = Notification.FLAG_AUTO_CANCEL;
-		//
-		// n.setLatestEventInfo(this, "Hello,there!",
-		// "Hello,there,I'm john.", null);
-		// nm.notify(1, n);
-
-	}
 }
